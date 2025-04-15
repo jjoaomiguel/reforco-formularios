b50dfa65464b35c09dcb278e185b792d7c6c5628
João Medeiros <joao_m_medeiros@estudante.sesisenai.org.br>
2025-04-15 12:37:04 -0300

    add - adicionando as primeiras colunas: primeiro nome, sobrenome, cpf, idade e qual seu PET

---
+++ script.js
@@ -0,0 +1,8 @@
+function validarFormulario() {   
+    
+    const nome = document.getElementById('nome_maquinista').value.trim();
+    const cpf = document.getElementById('cpf_maquinista').value.trim();
+    const idade = parseInt(document.getElementById('idade_maquinista').value, 10);
+    const experiencia = parseInt(document.getElementById('experiencia_maquinista').value, 10);
+
+}
